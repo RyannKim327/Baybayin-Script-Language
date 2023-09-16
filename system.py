f7834dbc265d746f3ec73ecd700917401a91856c
RyannKim327 <mpoprev2@email.comm>
2023-09-16 21:32:55 +0800

    9/16/2023 9:32:55 PM [Auto commit]

--- system.py
+++ system.py
@@ -2,8 +2,4 @@ reserved_keywords = [
 	"and", "except", "lambda", "with", "as", "finally", "nonlocal", "while", "assert", "false", "None", "yield", "break", "for", "not",
 	"class", "from", "or", "continue", "global", "pass", "def", "if", "raise", "del", "import", "return", "elif", "in", "True", "else", "is", "try"
 ]
-
-def test(text, dulo="\n", pagitan=" ", file=None, flush=False):
-	return f"print({test}, end='{dulo}', sep='{pagitan}', file='{file}', flush={flush})"
-
-print("test('hahaha')")
+print
