0de4aad78c0f02b2f9d25de14997b66cbdd82fb7
RyannKim327 <mpoprev2@email.comm>
2023-09-16 21:33:35 +0800

    9/16/2023 9:33:35 PM [Auto commit]

--- system.py
+++ system.py
@@ -1,6 +1,7 @@
-from bui
+from builtins import print as ilimbag
 reserved_keywords = [
 	"and", "except", "lambda", "with", "as", "finally", "nonlocal", "while", "assert", "false", "None", "yield", "break", "for", "not",
 	"class", "from", "or", "continue", "global", "pass", "def", "if", "raise", "del", "import", "return", "elif", "in", "True", "else", "is", "try"
 ]
-print()
+
+ilimbag("Kamusta")
