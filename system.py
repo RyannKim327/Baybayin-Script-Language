84d1f28e0eeec6adce4ac5e37a66bb0794b26973
RyannKim327 <mpoprev2@email.comm>
2023-09-16 22:27:58 +0800

    9/16/2023 10:27:58 PM [Auto commit]

--- system.py
+++ system.py
@@ -1,12 +1,12 @@
 from builtins import print as ilimbag, input as itala
 import os as _____
 
-reserved_keywords = [
-	"and", "except", "lambda", "with", "as", "finally", "nonlocal", "while", "assert", "false", "None", "yield", "break", "for", "not",
-	"class", "from", "or", "continue", "global", "pass", "def", "if", "raise", "del", "import", "return", "elif", "in", "True", "else", "is", "try"
-]
 
 def __magsalin__():
+	reserved_keywords = [
+		"and", "except", "lambda", "with", "as", "finally", "nonlocal", "while", "assert", "false", "None", "yield", "break", "for", "not",
+		"class", "from", "or", "continue", "global", "pass", "def", "if", "raise", "del", "import", "return", "elif", "in", "True", "else", "is", "try"
+	]
 	pass
 
 def scanner(file: str):
