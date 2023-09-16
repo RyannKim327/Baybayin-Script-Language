5c3ca9f68de5bcf186bc7bd6a19b933cc355a465
RyannKim327 <mpoprev2@email.comm>
2023-09-16 22:08:40 +0800

    9/16/2023 10:08:40 PM [Auto commit]

--- system.py
+++ system.py
@@ -6,4 +6,5 @@ reserved_keywords = [
 	"class", "from", "or", "continue", "global", "pass", "def", "if", "raise", "del", "import", "return", "elif", "in", "True", "else", "is", "try"
 ]
 
-def scanner(code: str):
+def scanner(file: str):
+	if _____.path.exists(file):
