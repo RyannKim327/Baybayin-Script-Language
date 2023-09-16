f043057da5bdbbaf480156dc312cfaf7c874c728
Ryannkim327 <mpoprev2@email.com>
2023-09-16 21:12:33 +0800

    9/16/2023 9:12:33 PM [Auto commit]

--- sample.py
+++ sample.py
@@ -1,3 +1,41 @@
 reserved_keywords = [
-	"True", "False"
+	and
+except
+lambda
+with
+as
+finally
+nonlocal
+while
+assert
+false
+None
+yield
+break
+for
+not
+
+class
+from
+or
+
+continue
+global
+pass
+
+def
+if
+raise
+
+del
+import
+return
+
+elif
+in
+True
+
+else
+is
+try
 ]
