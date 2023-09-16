bbc4b923b2480b18e9e74839f9e9941d3d18ebdc
RyannKim327 <mpoprev2@email.comm>
2023-09-16 21:31:15 +0800

    9/16/2023 9:31:15 PM [Auto commit]

--- system.py
+++ system.py
@@ -3,7 +3,7 @@ reserved_keywords = [
 	"class", "from", "or", "continue", "global", "pass", "def", "if", "raise", "del", "import", "return", "elif", "in", "True", "else", "is", "try"
 ]
 
-def test(text, **args):
-	return f"print({test}, end={dulo})"
+def test(text, dulo="\n", pagitan=" "):
+	return f"print({test}, end={dulo}, sep={})"
 
 exec("test(hahaha)")
