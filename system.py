391ac76cc65e2c01fddd5ced01a40535fa58dc38
RyannKim327 <mpoprev2@email.comm>
2023-09-16 22:32:04 +0800

    9/16/2023 10:32:04 PM [Auto commit]

--- system.py
+++ system.py
@@ -9,7 +9,7 @@ def __magsalin__():
 	]
 	tagalog = [
 		"at_saka", "maliban", "lambda", "kasama", "bilang", "sa_wakas", "nonlocal", "habang", "assert", "mali", "wala", "yield", "hinto", "para_sa", "hindi",
-		"class", "mula", "o", "sa_pagpapatuloy", "pangkalahatan", "pass", "def", "kung", "itaas", ""
+		"class", "mula", "o", "sa_pagpapatuloy", "pangkalahatan", "pass", "def", "kung", "itaas", "burahin", "ibilang", "ibalik", "o_kaya_ay", "sa"
 	]
 	__result__ = ""
 	
