93155475fca5da119dc97cad52e30e81b256efe9
RyannKim327 <mpoprev2@email.comm>
2023-09-16 22:08:05 +0800

    9/16/2023 10:08:05 PM [Auto commit]

--- system.py
+++ system.py
@@ -1,4 +1,5 @@
 from builtins import print as ilimbag, input as itala
+import os as _____
 
 reserved_keywords = [
 	"and", "except", "lambda", "with", "as", "finally", "nonlocal", "while", "assert", "false", "None", "yield", "break", "for", "not",
@@ -6,4 +7,3 @@ reserved_keywords = [
 ]
 
 def scanner(code: str):
-	
