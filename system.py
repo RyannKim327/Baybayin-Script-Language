1a8723db0a9217c8d1310e3fd8b360fb049559f7
RyannKim327 <mpoprev2@email.comm>
2023-09-16 21:28:55 +0800

    9/16/2023 9:28:55 PM [Auto commit]

--- system.py
+++ system.py
@@ -2,3 +2,5 @@ reserved_keywords = [
 	"and", "except", "lambda", "with", "as", "finally", "nonlocal", "while", "assert", "false", "None", "yield", "break", "for", "not",
 	"class", "from", "or", "continue", "global", "pass", "def", "if", "raise", "del", "import", "return", "elif", "in", "True", "else", "is", "try"
 ]
+
+def
