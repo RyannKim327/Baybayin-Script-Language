1c42efb84616445631990cd06bfc9b82ad34a1bd
Ryannkim327 <mpoprev2@email.com>
2023-09-16 21:15:33 +0800

    9/16/2023 9:15:33 PM [Auto commit]

--- sample.py
+++ sample.py
@@ -1,9 +1,5 @@
 reserved_keywords = [
 	"and", "except", "lambda", "with", "as", "finally", "nonlocal", "while", "assert", "false", "None", "yield", "break", "for", "not",
-	"class", "from", "or", "continue", "global", "pass", "def", "if", "raise",
-
-	"del",
-	"import",
-	"return",
- "elif", "in", "True", "else", "is", "try"
+	"class", "from", "or", "continue", "global", "pass", "def", "if", "raise", "del", "import", "return", "elif", "in", "True", "else", "is", "try"
 ]
+
