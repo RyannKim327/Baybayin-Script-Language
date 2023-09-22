917b226e909eab09763ac97e91a1c9551409a86f
RyannKim327 <mpoprev2@email.com>
2023-09-22 14:16:22 +0800

    9/22/2023 2:16:22 PM [Auto Commit]

--- system.py
+++ system.py
@@ -5,51 +5,50 @@ class system:
 	def __init__(self, file: str):
 		extension = ".pyby"
 		if not file.endswith(extension):
-			# return f"Hindi ito 'yong file na ginagamit para sa sawa"
+			ilimbag(f"Hindi ito 'yong file na ginagamit para sa sawa")
 		if _____.path.exists(file):
 			self.file = file
-			with open(file, "r") as f1:
-				return __magsalin__(f1.read())
 		else:
-			return f"Hindi ito 'yong file na ginagamit para sa sawa"
-
-def __magsalin__(__code__: str):
-	reserved_keywords = [
-		"and", "except", "lambda", "with", "as", "finally", "nonlocal", "while", "assert", "False", "None", "yield", "break", "for", "not",
-		"class", "from", "or", "continue", "global", "pass", "def", "if", "raise", "del", "import", "return", "elif", "in", "True", "else", "is", "try"
-	]
-	tagalog = [
-		"at", "maliban", "lambda", "kasama", "bilang", "sa_wakas", "nonlocal", "habang", "assert", "mali", "wala", "yield", "hinto", "para_sa", "hindi",
-		"class", "mula", "o_kaya_ay", "sa_pagpapatuloy", "pangkalahatan", "pass", "def", "kung", "itaas", "burahin", "ibilang", "ibalik", "o_kung_kaya", "sa", "tama", "kung_wala", "is", "subok"
-	]
-	__result__ = ""
-	__data__ = __code__.split("\n")
-	for __d__ in __data__:
-		__data2__ = __d__.split(" ")
-		for __d2__ in __data2__:
-			__key__ = __d2__.replace(":", "")
-			# if __d2__.startswith("(\""):
-			# 	__result__+= "(\""
-			# if __d2__.startswith("("):
-			# 	__result__+= "("
-			if __key__.replace(":", "").replace("(", "").replace(")", "") in tagalog:
-				for i in range(len(tagalog)):
-					if tagalog[i] == __key__.replace(":", "").replace("(", "").replace(")", ""):
-						__result__ += reserved_keywords[i]
-			else:
-				__result__ += __key__
-			
-			# if not __d2__.endswith("\")") and __d2__.endswith(")"):
-			# 	__result__+= ")"
-			# if  __d2__.endswith("\")"):
-			# 	__result__+= "\")"
-			if __d2__.endswith("):") and not __d2__.endswith("\"):"):
-				__result__+= ")"
-			if __d2__.endswith(":"):
-				__result__ += ":"
-			__result__ += " "
-		__result__ += "\n"
-	return __result__
-
-def __process__(file: str):
-	exec(__taga_sala__(file))
+			ilimbag(f"Hindi ito 'yong file na ginagamit para sa sawa")
+	
+	def __magsalin__(__code__: str):
+		reserved_keywords = [
+			"and", "except", "lambda", "with", "as", "finally", "nonlocal", "while", "assert", "False", "None", "yield", "break", "for", "not",
+			"class", "from", "or", "continue", "global", "pass", "def", "if", "raise", "del", "import", "return", "elif", "in", "True", "else", "is", "try"
+		]
+		tagalog = [
+			"at", "maliban", "lambda", "kasama", "bilang", "sa_wakas", "nonlocal", "habang", "assert", "mali", "wala", "yield", "hinto", "para_sa", "hindi",
+			"class", "mula", "o_kaya_ay", "sa_pagpapatuloy", "pangkalahatan", "pass", "def", "kung", "itaas", "burahin", "ibilang", "ibalik", "o_kung_kaya", "sa", "tama", "kung_wala", "is", "subok"
+		]
+		__result__ = ""
+		__data__ = __code__.split("\n")
+		for __d__ in __data__:
+			__data2__ = __d__.split(" ")
+			for __d2__ in __data2__:
+				__key__ = __d2__.replace(":", "")
+				# if __d2__.startswith("(\""):
+				# 	__result__+= "(\""
+				# if __d2__.startswith("("):
+				# 	__result__+= "("
+				if __key__.replace(":", "").replace("(", "").replace(")", "") in tagalog:
+					for i in range(len(tagalog)):
+						if tagalog[i] == __key__.replace(":", "").replace("(", "").replace(")", ""):
+							__result__ += reserved_keywords[i]
+				else:
+					__result__ += __key__
+				
+				# if not __d2__.endswith("\")") and __d2__.endswith(")"):
+				# 	__result__+= ")"
+				# if  __d2__.endswith("\")"):
+				# 	__result__+= "\")"
+				if __d2__.endswith("):") and not __d2__.endswith("\"):"):
+					__result__+= ")"
+				if __d2__.endswith(":"):
+					__result__ += ":"
+				__result__ += " "
+			__result__ += "\n"
+		return __result__
+	
+	def getResult(self):
+			with open(self.file, "r") as f1:
+				return self.__magsalin__(f1.read())
